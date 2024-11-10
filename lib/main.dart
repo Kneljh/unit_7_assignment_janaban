e40095c75099c71cb43ef0a6f61387beb85815b3
Kean Elijah Janaban <146790086+Kneljh@users.noreply.github.com>
2024-11-10 15:09:37 +0800

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Demon Slayer',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
